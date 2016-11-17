12ad29d9d28bc142789063f59353fdbe1b29e4e9
Diego Suarez and Eric Bomblatus <eric@>
2016-11-17 09:19:59 -0800

    code passes all 4 specs

--- lib/coin_combo.rb
+++ lib/coin_combo.rb
@@ -1,1 +1,22 @@
-
+class Fixnum
+  define_method(:get_change) do
+    change = [0,0,0,0]
+    counter = self
+    until counter == 0
+      if (counter / 25 >= 1)
+        change[0] = (counter / 25).floor
+        counter = counter - (change[0] * 25)
+      elsif (counter / 10 >= 1)
+        change[1] = (counter / 10).floor
+        counter = counter - (change[1] * 10)
+      elsif (counter / 5 >= 1)
+        change[2] = (counter / 5).floor
+        counter = counter - (change[2] * 5)
+      elsif (counter / 1 >= 1)
+        change[3] = (counter / 1).floor
+        counter = counter - (change[3] * 1)
+      end
+    end
+    change
+  end
+end
--- spec/coin_combo_spec.rb
+++ spec/coin_combo_spec.rb
@@ -3,6 +3,15 @@ require('coin_combo')
 
 describe ('Fixnum#coin_combo')do
   it('will return the least ammount of quarters for given ammount of cents') do
-    expect(50.get_change)to.eq([2,0,0,0])
+    expect(50.get_change).to(eq([2,0,0,0]))
+  end
+  it('will return the least ammount of quarters and dimes for given ammount of cents') do
+    expect(60.get_change).to(eq([2,1,0,0]))
+  end
+  it('will return the least ammount of quarters, dimes and nickels for given ammount of cents') do
+    expect(65.get_change).to(eq([2,1,1,0]))
+  end
+  it('will return the least ammount of quarters, dimes, nickels and pennies for given ammount of cents') do
+    expect(141.get_change).to(eq([5,1,1,1]))
   end
 end
