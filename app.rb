248d336ffa1769e09f37010b6bddf4f15d563a6b
Diego Suarez and Eric Bomblatus <diego@>
2016-11-17 13:09:22 -0800

    add sinatra

--- app.rb
+++ app.rb
@@ -9,16 +9,16 @@ end
 
 get('/output') do
    @input_change=params.fetch('input_change').to_i
-  # @array= @input_change.get_change([params.fetch('max_quarters').to_i,params.fetch('max_dimes').to_i,params.fetch('max_nickels').to_i,99999]
-  # @quarters = @array[0]
-  # @nickels = @array[1]
-  # @dimes = @array[2]
-  # @pennies = @array[3]
+  @array= @input_change.get_change([params.fetch('max_quarters').to_i,params.fetch('max_dimes').to_i,params.fetch('max_nickels').to_i,99999])
+  @quarters = @array[0]
+  @nickels = @array[1]
+  @dimes = @array[2]
+  @pennies = @array[3]
   @dollars=@input_change
-  @quarters = 101.get_change([5,5,5,133])
-  @nickels = "1"
-  @dimes = "1"
-  @pennies = "1"
+  # @quarters = 101.get_change([5,5,5,133])
+  # @nickels = "1"
+  # @dimes = "1"
+  # @pennies = "1"
 
   erb(:index)
 
